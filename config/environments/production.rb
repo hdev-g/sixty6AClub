da12d2ef4cf7dbf9cbda32b41ad599c9885ca193
Harris Grant <harris@shoptreen.com>
2023-07-31 11:54:48 +0100

    updates to production, adding https//: to solve production form error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,11 +66,11 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.default_url_options = { host: 'www.kinda.works' }
-  Rails.application.routes.default_url_options[:host] = 'www.kinda.works'
+  config.action_mailer.default_url_options = { host: 'https//:www.kinda.works' }
+  Rails.application.routes.default_url_options[:host] = 'https//:www.kinda.works'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default charset: 'utf-8'
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
