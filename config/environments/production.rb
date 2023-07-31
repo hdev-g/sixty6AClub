6ec270ea86d3d72ab948006a9b8a6cf01b63d358
Harris Grant <harris@shoptreen.com>
2023-07-31 11:41:11 +0100

    updated production environment to solve heroku form error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,8 +66,8 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer_default_url_options = { host: 'https://www.kinda.works' }
-  Rails.application.routes.default_url_options[:host] = 'https://www.kinda.works'
+  config.action_mailer.default_url_options = { host: 'www.kinda.works' }
+  Rails.application.routes.default_url_options[:host] = 'www.kinda.works'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
