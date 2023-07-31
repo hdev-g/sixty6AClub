b037fdcc16755c649d49bc1b50e63a1a9bb4c27c
Harris Grant <harris@shoptreen.com>
2023-07-31 14:14:08 +0100

    updated route for sign-in / sign-up to go straight to dashboard, and fixed flash messages above mapbox item

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,14 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :business])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :business])
+  end
+
+  def after_sign_in_path_for(resource)
+    user_dashboard_path
   end
 
   def set_markers
